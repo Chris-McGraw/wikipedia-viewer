2836df33dc4591c87f1174500346717667e5f518
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-02-04 16:37:19 -0500

    search-icon-test

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
   $("#wiki-search-button").on("click", function(){
     $("#results-container").slideToggle(750);
   });
+  $("#search-icon").on("click", function(){
+    $("#results-container").slideToggle(750);
+  });
 });
