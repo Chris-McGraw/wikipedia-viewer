614fdee62f0fcff5928e5e6e16b2b46d6af25c58
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-02-05 15:13:10 -0500

    script-update-1
    initial API get() call

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
+
+  /* $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&origin=*&srsearch=giant%20bomb", function(json) {
+    console.log(json.query.search[0].title);
+    console.log(json.query.search[0].snippet);
+
+    console.log(json.query.search[1].title);
+    console.log(json.query.search[2].title);
+    console.log(json.query.search[3].title);
+    console.log(json.query.search[4].title);
+    console.log(json.query.search[5].title);
+    console.log(json.query.search[6].title);
+    console.log(json.query.search[7].title);
+    console.log(json.query.search[8].title);
+    console.log(json.query.search[9].title);
+  }); */
+
   $("#wiki-search-button").on("click", function(){
     $("#results-container").slideToggle(750);
   });
