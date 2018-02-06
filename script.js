bf08252366c7273bf96535c487c8ada7e1fc9b9e
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-02-06 10:03:50 -0500

    script-update-3
    edits to functionality of Wiki API getJSON() call

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+  searchTerms = "";
+
   /* $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro&exsentences=1&explaintext&list=&generator=search&redirects=1&formatversion=2&origin=*&gsrsearch=giant%20bomb&gsrlimit=10", function(json) {
     json.query.pages.forEach(function(element){
       console.log(element.title);
@@ -9,8 +11,24 @@ $(document).ready(function(){
   }); */
 
   $("#wiki-search-button").on("click", function(){
-    $("#results-container").slideToggle(750);
+    searchTerms = document.getElementById("search-bar").value;
+
+    if(searchTerms !== "") {
+      console.log(searchTerms);
+    }
+
+    $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro&exsentences=1&explaintext&list=&generator=search&redirects=1&formatversion=2&origin=*&gsrsearch=giant%20bomb&gsrlimit=10", function(json) {
+      json.query.pages.forEach(function(element){
+        console.log(element.title);
+        console.log(element.extract);
+        console.log("");
+      });
+    });
   });
+
+  /* $("#wiki-search-button").on("click", function(){
+    $("#results-container").slideToggle(750);
+  }); */
 
   $("#search-icon").on("click", function(){
     $("#results-container").slideToggle(750);
