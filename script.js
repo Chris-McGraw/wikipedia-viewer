86fd930eacff16b6ea2339bcb8c7e618dcdcdfd9
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-02-06 14:11:09 -0500

    script-update-4
    edits to functionality of Wiki API getJSON() call

--- script.js
+++ script.js
@@ -2,33 +2,30 @@ $(document).ready(function(){
 
   searchTerms = "";
 
-  /* $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro&exsentences=1&explaintext&list=&generator=search&redirects=1&formatversion=2&origin=*&gsrsearch=giant%20bomb&gsrlimit=10", function(json) {
-    json.query.pages.forEach(function(element){
-      console.log(element.title);
-      console.log(element.extract);
-      console.log("");
-    });
-  }); */
-
   $("#wiki-search-button").on("click", function(){
+
     searchTerms = document.getElementById("search-bar").value;
 
     if(searchTerms !== "") {
       console.log(searchTerms);
-    }
 
-    $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro&exsentences=1&explaintext&list=&generator=search&redirects=1&formatversion=2&origin=*&gsrsearch=giant%20bomb&gsrlimit=10", function(json) {
-      json.query.pages.forEach(function(element){
-        console.log(element.title);
-        console.log(element.extract);
-        console.log("");
+      $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro&exsentences=1&explaintext&list=&generator=search&redirects=1&formatversion=2&origin=*&gsrsearch=" + searchTerms + "&gsrlimit=10", function(json) {
+
+        for(i = 0; i < 1; i++){
+          $("#results-container").append("<div class='result-frame-2'>" + json.query.pages[i].title + "</div>");
+        }
+
+        json.query.pages.forEach(function(element){
+          console.log(element.title);
+          console.log(element.extract);
+          console.log("");
+        });
       });
-    });
-  });
 
-  /* $("#wiki-search-button").on("click", function(){
-    $("#results-container").slideToggle(750);
-  }); */
+      /* $("#results-container").slideToggle(750); */
+
+    }
+  });
 
   $("#search-icon").on("click", function(){
     $("#results-container").slideToggle(750);
@@ -38,19 +35,5 @@ $(document).ready(function(){
     $("#results-container").empty();
     $("#results-container").append("No Results Found");
   }); */
-
-  $(".result-frame").on("mouseenter", function(){
-    $(this).addClass("hover-frame");
-  });
-  $(".result-frame").on("mouseleave", function(){
-    $(this).removeClass("hover-frame");
-  });
-
-  $(".result-frame-2").on("mouseenter", function(){
-    $(this).addClass("hover-frame");
-  });
-  $(".result-frame-2").on("mouseleave", function(){
-    $(this).removeClass("hover-frame");
-  });
 
 });
