a613faccf4e6457c2d7199834a946aa8ac26ba36
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-02-06 17:58:28 -0500

    script-update-5

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 
   $("#wiki-search-button").on("click", function(){
 
+    $("#results-container").empty();
     searchTerms = document.getElementById("search-bar").value;
 
     if(searchTerms !== "") {
@@ -11,8 +12,8 @@ $(document).ready(function(){
 
       $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro&exsentences=1&explaintext&list=&generator=search&redirects=1&formatversion=2&origin=*&gsrsearch=" + searchTerms + "&gsrlimit=10", function(json) {
 
-        for(i = 0; i < 1; i++){
-          $("#results-container").append("<div class='result-frame-2'>" + json.query.pages[i].title + "</div>");
+        for(i = 0; i < json.query.pages.length; i++){
+          $("#results-container").append("<div class='result-frame-2'>" + "<div class='result-title'>" + json.query.pages[i].title + "</div>" + "<div class='result-desc'>" + json.query.pages[i].extract + "</div>" + "</div>");
         }
 
         json.query.pages.forEach(function(element){
@@ -22,7 +23,7 @@ $(document).ready(function(){
         });
       });
 
-      /* $("#results-container").slideToggle(750); */
+      $("#results-container").slideToggle(750);
 
     }
   });
