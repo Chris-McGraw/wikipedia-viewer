aab10a20627ea6742dcd34647eddf753c9c8db01
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-02-07 11:53:21 -0500

    script-update-7
    edits to functionality of Wiki API getJSON() call

--- script.js
+++ script.js
@@ -11,15 +11,21 @@ $(document).ready(function(){
       $("#results-container").empty();
 
       $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro&exsentences=1&explaintext&list=&generator=search&redirects=1&formatversion=2&origin=*&gsrsearch=" + searchTerm + "&gsrlimit=10", function(json) {
-        for(i = 0; i < json.query.pages.length; i++){
-          $("#results-container").append("<div class='result-frame-2'>" + "<div class='result-title'>" + json.query.pages[i].title + "</div>" + "<div class='result-desc'>" + json.query.pages[i].extract + "</div>" + "</div>");
+        if(json.query) {
+          for(i = 0; i < json.query.pages.length; i++){
+            $("#results-container").append("<div class='result-frame-2'>" + "<div class='result-title'>" + json.query.pages[i].title + "</div>" + "<div class='result-desc'>" + json.query.pages[i].extract + "</div>" + "</div>");
+          }
+
+          /* json.query.pages.forEach(function(element){
+            console.log(element.title);
+            console.log(element.extract);
+            console.log("");
+          }); */
         }
 
-        json.query.pages.forEach(function(element){
-          console.log(element.title);
-          console.log(element.extract);
-          console.log("");
-        });
+        else {
+          $("#results-container").append("<div class='result-frame-2'>" + "<div class='result-none-title'>" + "No Results" + "</div>" + "</div>");
+        }
       });
 
       currentTerm = searchTerm;
