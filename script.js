167421f9cac48befcf9030e49de23d4bfa3a83ab
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-02-07 22:41:24 -0500

    script-update-8
    added wikipedia page ID links to corresponding #result-frames

--- script.js
+++ script.js
@@ -12,16 +12,16 @@ $(document).ready(function(){
 
       $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exintro&exsentences=1&explaintext&list=&generator=search&redirects=1&formatversion=2&origin=*&gsrsearch=" + searchTerm + "&gsrlimit=10", function(json) {
         if(json.query) {
-          for(i = 0; i < json.query.pages.length; i++){
-            $("#results-container").append("<div class='result-frame'>" + "<div class='result-title'>" + json.query.pages[i].title + "</div>" + "<div class='result-desc'>" + json.query.pages[i].extract + "</div>" + "</div>");
+          for(i = 0; i < json.query.pages.length; i++) {
+            $("#results-container").append("<a class='result-frame-link' target='_blank' href=" + "https://en.wikipedia.org/?curid=" + json.query.pages[i].pageid + ">" + "<div class='result-frame'>" + "<div class='result-title'>" + json.query.pages[i].title + "</div>" + "<div class='result-desc'>" + json.query.pages[i].extract + "</div>" + "</div>" + "</a>");
           }
 
-          json.query.pages.forEach(function(element){
+          /* json.query.pages.forEach(function(element){
             console.log(element.title);
             console.log(element.pageid);
             console.log(element.extract);
             console.log("");
-          });
+          }); */
         }
 
         else {
